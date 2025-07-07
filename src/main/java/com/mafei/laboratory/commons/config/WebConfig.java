885e4847daf60572cfb94054ec53da3cfb162485
黄暄 <3441578327@qq.com>
2025-07-07 22:49:27 +0800

    前端优化(图片回显)

---
+++ src/main/java/com/mafei/laboratory/commons/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.mafei.laboratory.commons.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 配置图片上传目录的访问路径
+        registry.addResourceHandler("/images/upload/**")
+                .addResourceLocations("file:D:/laboratory/upload/");  // 修改为你的实际上传目录
+    }
+} 
